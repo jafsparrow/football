ad5273ce70a92e2c29d48989969c8eee01d7159f
unknown <jafarcees@gmail.com>
2019-01-22 09:31:19 +0530

    pwa changes manifest.json

--- apps/timeout/src/app/app.component.ts
+++ apps/timeout/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { SideNavOption } from '@football/shared';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'football-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Timeout Sports';
   sideNavOptions: SideNavOption[] = [
     {
@@ -22,4 +23,16 @@ export class AppComponent {
       route: '/events/list'
     }
   ];
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(res => {
+        if (confirm('new version is available, Load new version?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
